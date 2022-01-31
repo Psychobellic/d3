239d1fb7023ced54e9602b257c241bc210772ee2
Matheus Schiffer <matheusschiffer@gmail.com>
2022-01-31 10:27:56 -0300

    constants

--- src/lessons/1-smileyFace.tsx
+++ src/lessons/1-smileyFace.tsx
@@ -3,30 +3,34 @@ import { Svg } from "../styles/1";
 const SmileyFace = () => {
   const height = 500;
   const width = 960;
+  const centerX = width / 2;
+  const centerY = height / 2;
   const strokeWidth = 10;
   const eyesOffset = 85;
+  const radius = height / 2 - strokeWidth / 2;
+
 
   return (
 		<Svg width={width} height={height}>
 			<circle
-				r={height / 2 - strokeWidth / 2}
-				cx={width / 2}
-				cy={height / 2}
+				r={radius}
+				cx={centerX}
+				cy={centerY}
 				fill="yellow"
 				stroke="black"
 				stroke-width={strokeWidth}
 			/>
 			<circle
-				r={height / 12}
-				cx={width / 2 - eyesOffset}
-				cy="175"
+				r={radius / 7}
+				cx={centerX - eyesOffset}
+				cy={centerY - 75}
 				stroke="black"
 				stroke-width={strokeWidth}
 			/>
 			<circle
-				r={height / 12}
-				cx={width / 2 + eyesOffset}
-				cy="175"
+				r={radius / 7}
+				cx={centerX + eyesOffset}
+				cy={centerY - 75}
 				stroke="black"
 				stroke-width={strokeWidth}
 			/>
