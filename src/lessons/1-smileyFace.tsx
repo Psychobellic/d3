419c7e1de110dce81864ffb1deb916e80cc5f6d5
Matheus Schiffer <matheusschiffer@gmail.com>
2022-01-31 10:52:46 -0300

    smiley face completed

--- src/lessons/1-smileyFace.tsx
+++ src/lessons/1-smileyFace.tsx
@@ -1,40 +1,50 @@
-import { Svg } from "../styles/1";
+import { G } from "../styles";
+import { arc } from 'd3';
 
 const SmileyFace = () => {
   const height = 500;
   const width = 960;
-  const centerX = width / 2;
-  const centerY = height / 2;
   const strokeWidth = 10;
-  const eyesOffset = 85;
+  const eyesOffsetX = 95;
+  const eyesOffsetY = 85;
   const radius = height / 2 - strokeWidth / 2;
+  const eyeRadius = radius / 7;
+  const mouthWidth = 15;
+  const mouthRadius = 175;
+  const margin = 0.15; // breaks half circle visual
 
+  const mouthArc = arc()
+		.innerRadius(mouthRadius)
+		.outerRadius(mouthRadius + mouthWidth)
+		.startAngle(Math.PI / 2 + margin)
+		.endAngle((Math.PI * 3) / 2 - margin);
 
   return (
-		<Svg width={width} height={height}>
-			<circle
-				r={radius}
-				cx={centerX}
-				cy={centerY}
-				fill="yellow"
-				stroke="black"
-				stroke-width={strokeWidth}
-			/>
-			<circle
-				r={radius / 7}
-				cx={centerX - eyesOffset}
-				cy={centerY - 75}
-				stroke="black"
-				stroke-width={strokeWidth}
-			/>
-			<circle
-				r={radius / 7}
-				cx={centerX + eyesOffset}
-				cy={centerY - 75}
-				stroke="black"
-				stroke-width={strokeWidth}
-			/>
-		</Svg>
+		<svg width={width} height={height}>
+			<G>
+				<circle
+					r={radius}
+					fill="yellow"
+					stroke="black"
+					stroke-width={strokeWidth}
+				/>
+				<circle
+					r={eyeRadius}
+					cx={- eyesOffsetX}
+					cy={- eyesOffsetY}
+					stroke="black"
+					stroke-width={strokeWidth}
+				/>
+				<circle
+					r={eyeRadius}
+					cx={eyesOffsetX}
+					cy={- eyesOffsetY}
+					stroke="black"
+					stroke-width={strokeWidth}
+				/>
+				<path d={mouthArc()} />
+			</G>
+		</svg>
 	);
 }
 
--- src/styles/1.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Svg = styled.svg`
-  transform: translate(15%,40%);
-`;
-
-
