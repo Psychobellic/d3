fc263c75e982c0970bb1aa8ff4d1fbbe034fc18f
Matheus Schiffer <matheusschiffer@gmail.com>
2022-02-02 15:34:37 -0300

    styled

--- src/lessons/4-barChart.tsx
+++ src/lessons/4-barChart.tsx
@@ -4,15 +4,17 @@ import { csv, scaleBand, scaleLinear, max, DSVRowArray } from 'd3';
 const BarChart = () => {
   const [data, setData] = useState<DSVRowArray>();
 
-  let width = 800;
-  let height = 600;
+  let width = 960;
+  let height = 500;
 
   useEffect(() => {
     const csvUrl =
       'https://gist.githubusercontent.com/Psychobellic/5ce889f0e43d5cb46bd5f3695f2177bc/raw/UN_Population_2019.csv';
-
     csv(csvUrl)
-			.then((data) => setData(data))
+			.then(d => {
+        setData(d.slice(0, 10));
+
+      })
 			.catch((e) => console.log(e));
   }, []);
 
@@ -24,7 +26,7 @@ const BarChart = () => {
 
   var yScale = scaleBand()
 		.domain(data.map(d => d.Country))
-		.range([0, height / 2]);
+		.range([0, height]);
 
   var xScale = scaleLinear()
 		.domain([0, max(data, (d: any) => d.Population)])
@@ -32,17 +34,14 @@ const BarChart = () => {
 
   return (
 		<svg width={width} height={height}>
-				{
-          data.map((item) => <rect
+				{data.map(item => <rect
             key={item.Index}
             x={0}
             y={yScale(item.Country)}
-            width={xScale(item.Population)}
+            width={xScale(item.Population) / 5}
             height={yScale.bandwidth()}
-            fill='#000'
           />
-          )
-        }
+          )}
 		</svg>
 	);
 }
