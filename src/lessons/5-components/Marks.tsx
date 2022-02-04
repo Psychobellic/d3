b343b3d5dfd9e0e9c2dcb7a54efcd9e4eca94ed0
Matheus Schiffer <matheusschiffer@gmail.com>
2022-02-04 10:37:02 -0300

    props

--- src/lessons/5-components/Marks.tsx
+++ src/lessons/5-components/Marks.tsx
@@ -1,16 +1,16 @@
 import { Circle } from "../../styles";
 
-const Marks = ({ data, yScale, xScale, yValue, xValue }: any) => {
-
-  return data.map((item: any) => (
+const Marks = ({ data, yScale, xScale, yValue, xValue, circleRadius }: any) => {
+	return data.map((item: any) => (
 		<Circle
 			cx={xScale(xValue(item))}
 			cy={yScale(yValue(item))}
-			r={8}
-		>
-			<title>Sepal Length: {xValue(item)} cm, {<br />}Sepal Width: {yValue(item)} cm</title>
+			r={circleRadius}>
+			<title>
+				Sepal Length: {xValue(item)} cm, {<br />}Sepal Width: {yValue(item)} cm
+			</title>
 		</Circle>
 	));
-}
+};
 
 export default Marks;
