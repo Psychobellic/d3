a94e8b36c3dc0016eb12970fe2d8382ff56b9de5
Matheus Schiffer <matheusschiffer@gmail.com>
2022-02-08 14:31:45 -0300

    onhover colors

--- src/lessons/8-components/colorLegend.tsx
+++ src/lessons/8-components/colorLegend.tsx
@@ -5,7 +5,9 @@ const ColorLegend = ({
 	tickSpacing,
 	tickSize,
 	textOffset,
-  onHover,
+	onHover,
+	hovered,
+	fadeOpacity,
 }: any) => {
 	return colorScale.domain().map((domainValue: any, i: number) => {
 		return (
@@ -16,11 +18,16 @@ const ColorLegend = ({
 					fontSize: '1.25rem',
 					fontFamily: 'Rowdies',
 					color: '#535353',
+					opacity: `${hovered && domainValue !== hovered ? fadeOpacity : 1}`,
 				}}
 				onMouseEnter={() => onHover(domainValue)}
-        onMouseOut={() => onHover(null)}>
-				<Circle fill={colorScale(domainValue)} r={tickSize} />
-				<text dy=".25em" x={textOffset}>
+				onMouseOut={() => onHover(null)}>
+				<Circle
+					fill={colorScale(domainValue)}
+					r={tickSize}
+					style={{ cursor: 'pointer' }}
+				/>
+				<text dy=".25em" x={textOffset} style={{ cursor: 'pointer' }}>
 					{domainValue.charAt(0).toUpperCase() + domainValue.slice(1)}
 				</text>
 			</ColorLegendG>
