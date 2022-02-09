d910d70f8bb66ea6939bacebfc7082c7070156b9
Matheus Schiffer <matheusschiffer@gmail.com>
2022-02-09 15:14:42 -0300

    created gist url

--- src/lessons/10-missingMigrants.tsx
+++ src/lessons/10-missingMigrants.tsx
@@ -1,7 +1,6 @@
 const MissingMigrants = () => {
-  return <>
-  
-  </>
+	// https://gist.githubusercontent.com/Psychobellic/359601fb9eff50be5b4c8ad9b1853487/raw/2022%2520Missing%2520Migrants%2520DataSet
+	return <></>;
 };
 
 export default MissingMigrants;
